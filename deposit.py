8dc8c3b6fe0b45e3216973671d9bbd51ff9ff6de
lrhel <lrhel@users.noreply.github.com>
2021-01-15 17:02:01 +0100

    Add deposit as Slash Command

--- deposit.py
+++ deposit.py
@@ -1,6 +1,9 @@
 from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
 import discord
 from discord.ext import commands
+from discord_slash import cog_ext
+from discord_slash import SlashCommand
+from discord_slash import SlashContext
 
 import user_db
 import config
@@ -9,10 +12,15 @@ import utility
 class Deposit(commands.Cog):
 
     def __init__(self, bot):
+        if not hasattr(bot, "slash"):
+            bot.slash = SlashCommand(bot, override_type=True, auto_register=True, auto_delete=True)
         self.bot = bot
+        self.bot.slash.get_cog_commands(self)
 
-    @commands.command()
-    async def deposit(self, ctx):
+    def cog_unload(self):
+        self.bot.slash.remove_cog_commands(self)
+
+    async def _deposit(self, ctx):
         client = AuthServiceProxy(f'http://{config.rpc_user}:{config.rpc_password}@{config.ip}:{config.rpc_port}')
         user_id = str(ctx.author.id)
         address = client.getaccountaddress(user_id)
@@ -23,9 +31,19 @@ class Deposit(commands.Cog):
             name=f'Send {config.currency} to this address.',
             value="Click to enlarge the QR code")
         embed.set_thumbnail(url=f'https://chart.googleapis.com/chart?cht=qr&chs=500x500&chl={address}')
+        return [embed, address]
 
-        await ctx.channel.send(embed=embed)
-        await ctx.channel.send(f'```{address}```')
+    @commands.command()
+    async def deposit(self, ctx):
+        r = await self._deposit(ctx)
+        await ctx.channel.send(embed=r[0])
+        await ctx.channel.send(f'```{r[1]}```')
+
+    @cog_ext.cog_slash(name="deposit", description=f'Get {config.currency} deposit address', guild_ids=config.guilds)
+    async def deposit_slash(self, ctx: SlashContext):
+        ctx.author = await self.bot.fetch_user(ctx.author)
+        r = await self._deposit(ctx)
+        await ctx.send(embeds=[r[0]], content=f'```{r[1]}```')
 
 def setup(bot):
     bot.add_cog(Deposit(bot))
